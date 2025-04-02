93e5fbc6a713b0012e01c09d62bd8b8036b0143d
OceaniFluctusGriseo <joshuabrown19994172@gmail.com>
2025-04-02 21:32:37 +0100

    Add next level button handler

--- menus/hud.gd
+++ menus/hud.gd
@@ -7,7 +7,7 @@ func _on_home_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
 
 func _on_next_level_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://levels/level%d.tscn" %CurrentLevel.value)
 
 func _on_level_win_hide() -> void:
 	$NextLevelButton.visible = false
